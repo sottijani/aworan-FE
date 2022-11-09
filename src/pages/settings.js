45ca2587836a6d663d45cea662e0cdf8f525a0aa
Afowowe Olumide Victor <volumide42@gmail.com>
2022-11-09 02:02:52 +0100

    complete setting page

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,38 +1,101 @@
+import { useState } from "react";
+import assets from "../js/assets";
+
 const Settings = () => {
+	const [tab, setTab] = useState("profile");
+
+	const changeTab = (v) => {
+		setTab(v);
+	};
+
 	return (
 		<div className="settings">
 			<p className="apt-4 mb-0 apb-4 font-700 font-22">Settings</p>
 
 			<div className="bg-white round-ter " style={{ minHeight: "76.2rem" }}>
 				<div className="tabs d-flex">
-					<span role="button">Profile</span>
-					<span role="button">Bank</span>
-					<span role="button">Security</span>
+					<span role="button" onClick={() => changeTab("profile")}>
+						Profile
+					</span>
+					<span role="button" onClick={() => changeTab("bank")}>
+						Bank
+					</span>
+					<span role="button" onClick={() => changeTab("security")}>
+						Security
+					</span>
 				</div>
 
-				<div className="container-fluid  content">
-					<div className="row">
-						<button className="col-md-3 round-ter d-flex flex-column justify-content-center align-items-center bank bg-white" data-bs-target="#addBank" data-bs-toggle="modal">
-							<i class="fa-solid fa-building-columns"></i>
-							<span className="py-1">Add bank account </span>
-						</button>
-						<div className="col-md-3 round-ter del-bank border-0 d-flex justify-content-between flex-column">
-							<p className="w-100 text-end text-danger font-400" data-bs-toggle="modal" data-bs-target="#exampleModal" role="button">
-								Delete
-							</p>
-							<p>
-								Jonathan Smith <br />
-								<small>0908 Access Bank</small>
-							</p>
+				{/* bank details */}
+				{tab === "bank" ? (
+					<div className="container-fluid  content">
+						<div className="row">
+							<button className="col-md-3 round-ter d-flex flex-column justify-content-center align-items-center bank bg-white" data-bs-target="#addBank" data-bs-toggle="modal">
+								<i class="fa-solid fa-building-columns"></i>
+								<span className="py-1">Add bank account </span>
+							</button>
+							<div className="col-md-3 round-ter del-bank border-0 d-flex justify-content-between flex-column">
+								<p className="w-100 text-end text-danger font-400" data-bs-toggle="modal" data-bs-target="#exampleModal" role="button">
+									Delete
+								</p>
+								<p>
+									Jonathan Smith <br />
+									<small>0908 Access Bank</small>
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
+				) : (
+					""
+				)}
+
+				{/* profile details */}
+				{tab === "profile" ? (
+					<div className="container-fluid  content">
+						<div className="row profile gap-0">
+							<SetForm title="Change Profile Picture" caption="Choose a new display picture for your account" component1={<img src={assets.avatar} width="100px" alt="" />} />
+							<SetForm title="Full Name" caption="Customize your account name" component1={<Input label="First Name" />} component2={<Input label="Last Name" />} />
+							<SetForm title="Email Address" caption="Change your email address" component1={<Input label="Email address" />} />
+							<SetForm title="Phone Number" caption="Change your Phone Number" component1={<Input label="Phone Number" />} />
+							<SetForm title="Social media" caption="Add social media accounts" component1={<Input label="Twitter" />} component2={<Input label="Instagram" />} />
+							<SetForm
+								component1={
+									<button type="button" className="d-bg-blue p-3 border-0 text-white round-ter w-50">
+										Save Changes
+									</button>
+								}
+							/>
+						</div>
+					</div>
+				) : (
+					""
+				)}
+
+				{/* security */}
+				{tab === "security" ? (
+					<div className="container-fluid  content">
+						<div className="row profile gap-0">
+							<SetForm title="Password" caption="Change your current Password" component1={<Input label="Twitter" type="password" />} />
+							<SetForm
+								component1={
+									<button type="button" className="d-bg-blue p-3 border-0 text-white round-ter w-50">
+										Save Changes
+									</button>
+								}
+							/>
+						</div>
+					</div>
+				) : (
+					""
+				)}
 			</div>
 
+			{/* modal */}
 			<div className="modal fade " id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" key="modal">
 				<div className="modal-dialog modal-dialog-centered ">
 					<div className="modal-content round-ter border-0  " style={{ padding: "2rem 3.5rem" }}>
-						<div className="modal-header border-0 font-700 font-22">Delete Bank Account</div>
+						<div className="modal-header border-0 font-700 font-22" style={{ color: " #323D59" }}>
+							Delete Bank Account
+						</div>
 						<div className="modal-body py-4">
 							<p>Are you sure you want to delete this bank account details?</p>
 						</div>
@@ -49,8 +112,10 @@ const Settings = () => {
 			<div className="modal fade addbank " id="addBank" tabIndex="-1" aria-labelledby="addBank" aria-hidden="true" key="bank">
 				<div className="modal-dialog modal-dialog-centered ">
 					<form className=" w-100 round-ter border-0 ">
-						<div className="modal-content  border-0 p-5 w-75 mx-auto">
-							<div className="modal-header border-0 font-700 font-22 ">Add Bank Account</div>
+						<div className="modal-content  border-0 p-4 w-75 mx-auto">
+							<div className="modal-header border-0 font-700 font-22 " style={{ color: " #323D59" }}>
+								Add Bank Account
+							</div>
 							<div className="modal-body py-3">
 								<div className="py-3">
 									<label className="d-block my-2">Bank</label>
@@ -59,6 +124,9 @@ const Settings = () => {
 								<div className="py-3">
 									<label className="d-block my-2">Account Number</label>
 									<input className="p-3" type="number" required />
+									<small className="text-end d-block py-3 font-500" style={{ color: "#05A86D" }}>
+										Olawale Amara Galadima
+									</small>
 								</div>
 							</div>
 							<div className="modal-header border-0 d-flex gap-3 bt ">
@@ -78,3 +146,27 @@ const Settings = () => {
 };
 
 export default Settings;
+
+const SetForm = ({ title, caption, component1, component2 }) => {
+	return (
+		<>
+			<div className="col-md-3  d-flex flex-column justify-content-center pb-4 pe-5">
+				<p className="m-0" style={{ color: "#323D59" }}>
+					{title}
+				</p>
+				<small>{caption}</small>
+			</div>
+			<div className="col-md-4 py-4 ps-0 pe-5">{component1}</div>
+			<div className="col-md-4 py-4 ps-0 pe-5">{component2}</div>
+		</>
+	);
+};
+
+const Input = ({ label, type = "text", ...other }) => {
+	return (
+		<div>
+			<label className="d-block mb-2">{label}</label>
+			<input type={type} className="w-100 p-3" {...other} />
+		</div>
+	);
+};
